4ec62c90d03911411e3dee36d28e051d771f84bb
tarek <tarekbozo@gmai.com>
2021-12-04 22:37:57 +0100

    first commit

---
+++ src/components/Modal.js
@@ -0,0 +1,58 @@
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+import imgModal from './modal.jpg'
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+`;
+
+const ModalImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  background: #000;
+`;
+
+
+
+export const Modal = ({ showModal, setShowModal }) => {
+  const modalRef = useRef();
+
+  const closeModal = e => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  
+
+  return (
+    <>
+      {showModal ? (
+        <Background onClick={closeModal} ref={modalRef}>
+            <ModalWrapper showModal={showModal}>
+              <ModalImg src={imgModal} />
+            </ModalWrapper>
+        </Background>
+      ) : null}
+    </>
+  );
+};
